d682cb9e65d8b613f9524442856d75d1615961d9
Mechelle <mechellepresnell@gmail.com>
2026-08-04 12:35:12 -0400

    Added contitional rendering, todo completion, and controlled form

--- src/TodoList.jsx
+++ src/TodoList.jsx
@@ -1,11 +1,19 @@
 import TodoListItem from './TodoListItem.jsx'
 
-function TodoList({todoList}) {
+function TodoList({todoList, onCompleteTodo}) {
+  const filteredTodoList = todoList.filter((todo) => !todo.isCompleted);
 
+// this will produce either a message or the list
     return (
+      filteredTodoList.length === 0 ? (
+        <p> Add todo above to get started</p>
+      ) : (
       <ul>
-        {todoList.map(todo => <TodoListItem key={todo.id} todo={todo} />)}
-      </ul>
+        {filteredTodoList.map((todo) => (
+          <TodoListItem key={todo.id} todo={todo} onCompleteTodo={onCompleteTodo} />
+        ))}
+      </ul> 
+      )
     );
 }
 

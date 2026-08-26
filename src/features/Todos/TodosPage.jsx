1b32683172ed57fc4fcb109009a3ddf43e401035
Mechelle <mechellepresnell@gmail.com>
2026-08-26 09:10:04 -0400

    fixed colon missing

--- src/features/Todos/TodosPage.jsx
+++ src/features/Todos/TodosPage.jsx
@@ -55,7 +55,7 @@ export default function TodosPage({ token }) {
 
         } catch (error) {
             if(debouncedFilterTerm || sortBy !== 'createdAt' || sortDirection !== 'desc') {
-                setFilterError(`Error filtering/sorting todos ${error.message}`);
+                setFilterError(`Error filtering/sorting todos: ${error.message}`);
             } else {
                 setError(`Error fetching todos: ${error.message}`);
             }

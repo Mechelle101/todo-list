b4babbed688e363941958a78359e0c22316c506e
Mechelle <mechellepresnell@gmail.com>
2026-09-07 18:53:10 -0400

    add Navigation with NavLink active styling and logout redirect

--- src/features/Logoff.jsx
+++ src/features/Logoff.jsx
@@ -1,16 +1,20 @@
 import { useState } from "react";
+import { useNavigate } from "react-router";
 import { useAuth } from "../contexts/AuthContext.jsx";
 
 export default function Logoff() {
   const [error, setError] = useState("");
 
   const { logout } = useAuth();
+  const navigate = useNavigate();
 
   const handleLogoff = async () => {
     setError("");
 
     const result = await logout();
-    if (!result.success) {
+    if (result.success) {
+      navigate("/login");
+    } else {
       setError(result.error);
     }
   };
